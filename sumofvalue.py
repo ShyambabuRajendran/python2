a1d1d8031112a6ebec40ba4b3d1febcfabf2d07d
ShyambabuRajendran <shyambabu3628@gmail.com>
2024-11-28 22:38:59 +0530

    new python files

---
+++ sumofvalue.py
@@ -0,0 +1,12 @@
+sum=0
+for i in range (0, 15+1):
+    sum = sum + i
+    i= i+1
+print (" Sum =", sum)
+    # i =0, sum = 0+0
+    #i=1, sum = 0+1
+    #i=2, sum = 1+2
+    #i=3, sum = 3+3
+    #i=4, sum = 6+4
+    #i=5, sum = 10+5
+    
---
+++ swap.py
@@ -0,0 +1,7 @@
+a=int(input("enter the a value"))
+b=int(input("enter the b value"))
+print("a=",a,"b=",b)
+a= a+b
+b= a-b
+a= a-b
+print("after swapping a=",a,"b=",b)
